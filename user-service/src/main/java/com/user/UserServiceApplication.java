c5bb3dfd729bd292db9e12096b687efa3a2b5b3c
Dhiraj Patil <dhiraj.patil413@gmail.com>
2022-09-27 11:26:30 +0530

    added department service and properties file changes

--- user-service/src/main/java/com/user/UserServiceApplication.java
+++ user-service/src/main/java/com/user/UserServiceApplication.java
@@ -1,9 +1,10 @@
-package com.user.service;
+package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.user.*"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
---
+++ user-service/src/main/java/com/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("getUser")
+	public String getUser() {
+		return "Dhiraj Patil";
+	}
+}
