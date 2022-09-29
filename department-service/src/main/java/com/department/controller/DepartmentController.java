60bb24ee1ff392c6da143a3af5e5aca77793a8fc
Dhiraj Patil <dhiraj.patil413@gmail.com>
2022-09-30 02:24:31 +0530

    Implementation of circuit breaker in user-service

--- department-service/src/main/java/com/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/department/controller/DepartmentController.java
@@ -6,19 +6,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController {
-	
+
 	@Value("${server.port}")
 	private int serverPort;
-	
+	private static final String SERVICE_NAME = "departmentService";
+
 	@GetMapping("/{id}")
 	public String findDepartmentById(@PathVariable("id") Long departmentId) {
 		return "This is department service";
 	}
+
+	@CircuitBreaker(name=SERVICE_NAME,fallbackMethod="departmentServiceFallback")
 	@GetMapping("/department")
 	public String getUserDeparment() {
 		return "This is department service using eureka " + serverPort;
+	}
+	
+	public String departmentServiceFallback(Exception e) {
+		return "Department service is down.";
+		
 	}
 }
--- user-service/src/main/java/com/user/controller/UserController.java
+++ user-service/src/main/java/com/user/controller/UserController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
 	@Autowired
 	private RestTemplate restTemplate;
+	private static final String SERVICE_NAME = "userService";
 
 	@GetMapping("/getUser")
 	public String getUser() {
@@ -19,9 +22,16 @@ public class UserController {
 	}
 
 	@GetMapping("/getdepartment")
+	@CircuitBreaker(name = SERVICE_NAME, fallbackMethod = "userServiceFallback")
 	public String getUserDepartment() {
 		String result = restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/department", String.class);
 		return result;
+	}
+
+	
+	public String userServiceFallback(Exception e) {
+		return "Department service is down.";
+
 	}
 
 }
