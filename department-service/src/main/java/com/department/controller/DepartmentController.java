f6859ec8cdd05e987e28b13bf5fc21008e7588cf
Dhiraj Patil <dhiraj.patil413@gmail.com>
2022-09-30 01:36:29 +0530

    Code changes department controller

--- department-service/src/main/java/com/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/department/controller/DepartmentController.java
@@ -1,5 +1,6 @@
 package com.department.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController {
+	
+	@Value("${server.port}")
+	private int serverPort;
+	
 	@GetMapping("/{id}")
 	public String findDepartmentById(@PathVariable("id") Long departmentId) {
 		return "This is department service";
 	}
 	@GetMapping("/department")
 	public String getUserDeparment() {
-		return "This is department service using eureka";
+		return "This is department service using eureka " + serverPort;
 	}
 }
